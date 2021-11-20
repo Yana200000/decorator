5a130dbe451c511acd364cea809c8a462b33db04
Yana <riva_yana@mail.ru>
2021-11-20 18:57:22 +0300

    first

--- function.py
+++ function.py
@@ -1,11 +1,12 @@
 import datetime
-
+import requests as rq
 from main import write_data_and_time
 
 
 @write_data_and_time
 def func(*args):
-    return args
+    return f'{args}'
 
 
-print(func(1, 2, 3))
+if __name__ == '__main__':
+    print(func(1, 2, 8))
--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from datetime import datetime
 def write_data_and_time(old_function):
     def new_function(*args):
         file = open('new_file.txt', 'w+')
-        file.write(f'Дата и время вызова функции: {datetime.today().strftime("%Y-%m-%d-%H.%M.%S")}; имя функции: {old_function}; аргументы функции: {args}; значение: {old_function()}')
+        func_name = old_function.__name__
+        file.write(f'Дата и время вызова функции: {datetime.today().strftime("%Y-%m-%d-%H.%M.%S")}\n'
+                   f'Имя функции: {func_name}\n'
+                   f'Аргументы функции: {args}\n'
+                   f'Значение: {old_function}')
 
         return f'Информация добавлена'
     return new_function
